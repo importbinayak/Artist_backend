ca46a5a7248efed4f4c827bc1314cf3ef3bf269e
Binayak Dhungana <thedhungana478@gmail.com>
2025-07-20 22:50:38 +0545

    setup

--- art_backend/settings.py
+++ art_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,29 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+SIMPLE_JWT={
+    'ACCESS_TOKEN_LIFETIME':timedelta(days=30),
+    'REFRESH_TOKKEN_LIFETIME':timedelta(days=180),
+    'ROTATE_REFRESH_TOKENS':False,
+}
+
+REST_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
+CORS_ALLOWED_ORIGIN=[
+    "http://127.0.0.1:5173"
+]
+
 # Application definition
+
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,11 +60,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangorestframework',
+    'djangorestframework_simplejwt',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
