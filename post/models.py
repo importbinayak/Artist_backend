bdc99c17b12eb53ca5ef9cb7f79fb9e0fc0fcf89
Binayak Dhungana <thedhungana478@gmail.com>
2025-07-25 22:59:51 +0545

    post ko db banako

---
+++ post/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+import uuid
+from account.models import User
+
+
+# Create your models here.
+
+class PostAttachment(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    image=models.ImageField(upload_to='post_attachments')
+    created_by=models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    body=models.TimeField(blank=True,null=True)
+    
+    attachments=models.ManyToManyField(PostAttachment,blank=True,null=True)
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
