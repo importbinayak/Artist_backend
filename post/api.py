1016907555df5fa330641d6c3c88a759f7b45599
Binayak Dhungana <thedhungana478@gmail.com>
2025-07-26 21:50:39 +0545

    hernu parne xa

--- post/api.py
+++ post/api.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework.decorators import api_view,authentication_classes,permission_classes
+from .forms import PostForm
 
 # Create your views here.
 
@@ -12,3 +13,17 @@ def post_list(request):
     posts=Post.objects.all()
     serializer=PostSerializer(posts,many=True)
     return JsonResponse({'data':serializer.data})
+
+
+@api_view(['POST'])
+def post_create(request):
+    form=PostForm(request.data)
+    if form.is_valid():
+        post=form.save(commit=False)
+        post.created_by=request.user
+        post.save()
+        
+        serializer=PostSerializer(post)
+        return JsonResponse(serializer.data,safe=False)
+    else:
+        return JsonResponse({'error':'hello says hello'})
--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .api import post_list
+from .api import post_list,post_create
 
 
 urlpatterns = [
     path('',post_list,name='post'),
+    path('create/',post_create,name='post_create'),
 ]
