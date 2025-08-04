e40007d7df3983dc4097526835ca1a0814ad1e96
Binayak Dhungana <thedhungana478@gmail.com>
2025-08-04 22:24:23 +0545

    comment model banako tara migrate garna baki xa

--- post/models.py
+++ post/models.py
@@ -12,6 +12,13 @@ class Likes(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
 
 
+class Comment(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    body=models.TextField(blank=True,null=True)
+    created_by=models.ForeignKey(User,related_name='comments',on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+
 class PostAttachment(models.Model):
     id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     image=models.ImageField(upload_to='post_attachments')
@@ -28,6 +35,9 @@ class Post(models.Model):
     
     likes=models.ManyToManyField(Likes,blank=True)
     likes_count=models.IntegerField(default=0)
+    
+    comments=models.ManyToManyField(Comment,blank=True)
+    comments_count=models.IntegerField(default=0)
     
     created_by=models.ForeignKey(User,related_name='posts',on_delete=models.CASCADE)
     
